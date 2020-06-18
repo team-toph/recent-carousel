1ee73fecaafc6ef5644059fd60e71f7078281200
Shraya Chakma <shrayachakma@gmail.com>
2020-06-17 21:24:13 -0700

    Added dependencies

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/App.jsx',
+  mode: 'development',
+  watch: true,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+
+        }
+      }
+    ]
+  }
+};
