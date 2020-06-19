a6e196e9ca231a74bf1c424f10fbbc256977f31d
Shraya Chakma <shrayachakma@gmail.com>
2020-06-19 09:52:09 -0700

    Added a get req

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const Views = require('../database/View.js');
+const db = require('../database/index');
+
 const app = express();
 const PORT = 3000;
 
@@ -11,6 +14,16 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.get('/', function(req, res) {
   res.send('Hello World!')
 });
+
+app.get('/api/viewedItems', function(req, res) {
+  Views.find(function(err, data) {
+    if (err) {
+      console.log('get error in server', err);
+    } else {
+      res.json(data);
+    }
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
