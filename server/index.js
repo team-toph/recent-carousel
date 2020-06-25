25f19d30178861a74d8f7665487d9f9f2ed2a929
Shraya Chakma <shrayachakma@gmail.com>
2020-06-24 18:50:01 -0700

    Updated with tests

--- server/index.js
+++ server/index.js
@@ -9,22 +9,25 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/../dist'));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!')
+// });
 
 app.get('/api/recentlyViewed', function(req, res) {
   Views.find(function(err, data) {
     if (err) {
       console.log('get error in server', err);
     } else {
-      res.json(data);
+      res.status(200).json(data);
     }
   })
 })
 
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+module.exports = app;
---
+++ test/server.test.js
@@ -0,0 +1,31 @@
+///////////// example test from jest docs /////////////
+const sum = require('../server/sum.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+///////////////////////////////////////////////////////
+
+const request = require('supertest');
+const app = require('../server/index.js');
+const mongoose = require('mongoose');
+
+describe('Test to see if api endpoint / works', () => {
+  test('It should recieve a status code of 200', async () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.status).toBe(200);
+      });
+  });
+});
+
+describe('Test to see if api endpoint /recentlyViewed works', () => {
+  test('It should recieve a status code of 200', async () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.status).toBe(200);
+      });
+  });
+});
