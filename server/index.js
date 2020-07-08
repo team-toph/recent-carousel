ffcbc519d08d24fa0ca5c97fd9c54011e6accbf3
Robert Bessler <peanutjackreigns@gmail.com>
2020-07-08 15:25:34 -0400

    Adds Update/put route to server

--- server/index.js
+++ server/index.js
@@ -24,6 +24,13 @@ app.get('/api/products', function(req, res) {
     })
 });
 
+app.put('/api/products', (req, res) => {
+  var id = req.query.id;
+  View.findOneAndUpdate({id: id}, req.body, {new: true})
+    .then((results) => {
+      res.status(200).json(results._doc);
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
