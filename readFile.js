d503391d6887fb280c95cbbfa5db2cd17609b9f1
Robert Bessler <peanutjackreigns@gmail.com>
2020-07-10 17:53:19 -0400

    Completes script to load 10M records in 58 minutes

--- readFile.js
+++ readFile.js
@@ -1,24 +1,38 @@
 module.exports = (callback, cb2) => {
   var fs = require('fs');
-  var stream = fs.createReadStream('./data2.txt');
+  var stream = fs.createReadStream('./data.txt', {encoding: 'utf8'});
   var items = [];
+  var chunks = [];
+  var done = false;
   var leftover = '';
   stream.on('data', (chunk) => {
-    stream.pause();
-    items = items.concat((leftover + chunk.toString()).split('\r\n'));
-    leftover = items.pop();
-    if (items.length > 10000 || leftover === '') {
+    chunks.push(chunk);
+    if (chunks.length > 300) {
+      stream.pause();
+      items = chunks.reduce((acc, chunk) => {
+        return acc + chunk;
+      }, leftover).split('\r\n');
+      leftover = items.pop();
       items = items.map((item) => {
         return JSON.parse(item);
       });
       callback(items, () => stream.resume());
       items = [];
-    } else {
-      stream.resume();
+      chunks = [];
     }
   });
 
   stream.on('end', () => {
+    chunks.forEach((chunk) => {
+      items = items.concat((leftover + chunk.toString()).split('\r\n'));
+      leftover = items.pop();
+    });
+    items = items.map((item) => {
+      return JSON.parse(item);
+    });
+    callback(items, () => stream.resume());
+    items = [];
+    chunks = [];
     cb2();
   });
 };
