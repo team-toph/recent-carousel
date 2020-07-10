d349ae98908682a9e091af89df03904f16da3159
Robert Bessler <peanutjackreigns@gmail.com>
2020-07-09 23:05:01 -0400

    Adds 10k records in 3.5s barely fast enough

--- maria/seed.js
+++ maria/seed.js
@@ -4,11 +4,8 @@ var { sequelize, Item, Product } = require('./index.js');
 var done = false;
 var start = Date.now();
 
-var paused = false;
-
 read((items, callback) => {
   var products = [];
-  // console.log(item.id);
   items.forEach((item) => {
     item.product.forEach((product) => {
       product.itemId = item.id;
@@ -21,35 +18,16 @@ read((items, callback) => {
     })
     .then(() => {
       console.log(`Added ${items.length} items. Total time: ${Date.now() - start}ms`);
-      callback();
       if (done) {
         console.log(`Time to add all: ${Date.now() - start}ms`);
         sequelize.close();
+      } else {
+        callback();
       }
     })
     .catch((err) => {
       console.log(err);
-    })
-  // items.push(item);
-  // item.product.forEach((thing) => {
-  //   thing.itemId = item.id;
-  //   products.push(thing);
-  // });
-  // if (items.length >= 10 && !paused) {
-  //   callback(true);
-  //   Item.bulkCreate(items)
-  //   //  .then(() => {
-  //   //    return Product.bulkCreate(products);
-  //   //   })
-  //     .then(() => {
-  //       console.log('Done adding 10k, time taken: ', Date.now() - start);
-  //       items = [];
-  //       callback(false);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.code);
-  //     })
-  // }
+    });
 }, () => {
   done = true;
 });
--- readFile.js
+++ readFile.js
@@ -1,22 +1,21 @@
 module.exports = (callback, cb2) => {
   var fs = require('fs');
   var stream = fs.createReadStream('./data2.txt');
+  var items = [];
   var leftover = '';
   stream.on('data', (chunk) => {
     stream.pause();
-    var items = [];
     items = items.concat((leftover + chunk.toString()).split('\r\n'));
     leftover = items.pop();
-    items = items.map((item) => {
-      return JSON.parse(item);
-    });
-    callback(items, () => stream.resume());
-    // while (items.length > 0) {
-    //   var item = JSON.parse(items.pop());
-    //   callback(item, (bool) => {
-    //     bool ? stream.pause() : stream.resume();
-    //   });
-    // }
+    if (items.length > 10000 || leftover === '') {
+      items = items.map((item) => {
+        return JSON.parse(item);
+      });
+      callback(items, () => stream.resume());
+      items = [];
+    } else {
+      stream.resume();
+    }
   });
 
   stream.on('end', () => {
