fa5440be3e255ce41dde8edf7fe7ddd733d55747
Robert Bessler <peanutjackreigns@gmail.com>
2020-07-14 15:21:03 -0400

    Fixes Create and Read routes of CRUD API

--- maria/index.js
+++ maria/index.js
@@ -1,22 +1,31 @@
-var { Sequelize, DataTypes } = require('sequelize');
-var { user, pass } = require('./config.js');
-var sequelize = new Sequelize('SDC', user, pass, {
+var mariadb = require('mariadb');
+var config = require('./config.js');
+var pool = mariadb.createPool({
   host: 'localhost',
-  dialect: 'mariadb',
-  logging: false
+  user: config.user,
+  password: config.pass,
+  database: 'SDC'
 });
-var modelOptions = { timestamps: false };
 
-var Item = sequelize.define('item', {}, modelOptions);
+var find = function(id) {
+  return pool.query(`SELECT * FROM products where itemId = ${id}`)
+    .then((product) => {
+      return [{ id, product }];
+    })
+    .catch((err) => {
+      console.log('error retrieving item: ', err);
+    });
+};
 
-var Product = sequelize.define('product', {
-  cost: DataTypes.INTEGER,
-  imageUrl: DataTypes.STRING,
-  name: DataTypes.STRING,
-  ratings: DataTypes.INTEGER,
-  reviewsCount: DataTypes.INTEGER
-}, modelOptions);
+var create = function(item) {
+  return pool.query(`INSERT INTO items VALUES (${item.id})`)
+    .then(() => {
+      item.product = item.product.map((product) => Object.values(product));
+      return pool.batch(`INSERT INTO products(reviewsCount, ratings, cost, imageUrl, name, itemId) VALUES (?, ?, ? ,?, ?, ${item.id})`, item.product);
+    })
+    .catch((err) => {
+      console.log('error adding item: ', err);
+    });
+};
 
-Item.hasMany(Product);
-
-module.exports = { Item, Product, sequelize };
+module.exports = { pool, find, create };
